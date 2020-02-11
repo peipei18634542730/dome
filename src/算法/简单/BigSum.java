70ccfba59c4ed10800d4d25ef217dd1d7b101b33
peipei18634542730 <liupeidong320163@163.com>
2020-02-11 21:14:57 +0800

    算法 - 求数组的最大子序之和

--- src/算法/简单/BigSum.java
+++ src/算法/简单/BigSum.java
@@ -10,7 +10,8 @@ package 算法.简单;
 
 public class BigSum {
     /**
-     * 解题思路：
+     * 解题思路：//如果数组都是负值，返回的是最大的负数。如果有正有负，返回的是最大的子序之和。
+     * 不要想错了，如果数组中最大的正整数左右都是负数，那结果肯定是这个整数。不要考虑加左边还是加右边
      *          动态规划
      *          1.如果sum>0,说明当前的结果对增益有效果，将其与遍历的数字相加
      *          2.如果sum<=0,说明当前结果对增益无效，sum直接更新为当前遍历的数
@@ -24,13 +25,13 @@ public class BigSum {
             if (sum>0) {
                 sum += num;
             } else {
-                sum = num;//第一次，sum变成数组第一个数
+                sum = num;//第一次，sum变成数组第一个数。
             }
-            ans = Math.max(sum,ans);
+            ans = Math.max(sum,ans);//第一次ans还是第一个数。循环找正整数，如果没有则返回最大的负数。如果有则一次循环相加，ans始终是取的最大的那个
         }
         return ans;
     }
-    //如果数组都是负值，返回的是最大的负数。如果有正有负，返回的是最大的子序之和。
+
 
     public static void main(String[] args) {
         BigSum bigSum = new BigSum();
